a258580782fc05e279582abf705281b26d7a05ce
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-30 16:24:54 +0000

    fabric scrip to create .tgz file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""A Fabric script to create .tgz file"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """Generate a .tgz archive from
+    the contents of the web_static"""
+    dt = datetime.now()
+    file_name = 'web_static_{}{}{}{}{}{}.tgz'.format(
+            dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second)
+    try:
+        if not os.path.exists("versions"):
+            os.mkdir("versions")
+        local("tar -cvzf versions/{}".format(
+            file_name))
+        return os.path.join("versions", file_name)
+    except Exception as e:
+        return None

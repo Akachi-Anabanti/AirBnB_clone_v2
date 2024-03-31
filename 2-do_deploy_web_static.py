4cab2d6b557553ca84ff219c430dea314a1e54c2
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-03-31 00:53:12 +0000

    pycodefix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,16 +36,15 @@ def do_deploy(archive_path):
         put(filename, '/tmp/')
         name = filename.split(".")[0]
 
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            filename, name
-            ))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run(f"mkdir -p /data/web_static/releases/{name}/")
+        run(f"tar -xzf /tmp/{filename} -C /data/web_static/releases/{name}/")
+        run(f"rm /tmp/{name}")
+        run(f"""mv /data/web_static/releases/{name}/web_static/*
+                 /data/web_static/releases/{name}""")
+        run(f"rm -rf /data/web_static/releases/{name}/web_static")
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(name))
+        run(f"""ln -s /data/web_static/releases/{name}/
+                 /data/web_static/current""")
         return True
     except Exception as e:
         return False

4e79847a53faf8f37bc7d3186c25c552a2086a71
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-04-09 23:34:02 +0000

    Base class

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import String, Column
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """Represents the Amenities Table"""
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)

7b0ad783818441eb54177e00481db519e22657c2
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-04-09 12:24:23 +0000

    integer parameter

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A Simple flask app that serves a text"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    """Home Page"""
+    return("HBNB!")
+
+
+@app.route("/hbnb")
+def hbnb():
+    """Hbnb Page"""
+    return("HBNB")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def show_text(text):
+    """shows the text"""
+    return "C" + " " + escape(text).replace("_", " ")
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """Show python text"""
+    return "Python" + " " + escape(text).replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_number(n):
+    """Displays int variable"""
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

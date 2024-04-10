bee529d348ad04d81a8ac88e376584199bbbd4f5
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-04-10 16:13:41 +0000

    places and filters

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""A flask application"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Defines page filters"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    places = storage.all("Place")
+    return render_template("100-hbnb.html", states=states,
+                           amenities=amenities,
+                           places=places)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """tear down"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
